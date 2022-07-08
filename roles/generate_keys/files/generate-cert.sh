93f0eeb9cb6a08543e6c4ad3dc9e105f682cb43d
nocturnalq <leonidfilippov256@gmail.com>
2022-07-08 22:53:15 +0400

    added certs

--- roles/generate_keys/files/generate-cert.sh
+++ roles/generate_keys/files/generate-cert.sh
@@ -9,3 +9,6 @@ pki --pub --in /etc/ipsec.d/private/$1-key.pem --type rsa \
         --dn "CN=$1.patchka.vsfi" --san $1.patchka.vsfi \
         --flag serverAuth --flag ikeIntermediate --outform pem \
     >  /etc/ipsec.d/certs/$1-cert.pem
+
+scp -oStrictHostKeyChecking=no -i /root/.ssh/vsfikey /etc/ipsec.d/private/$1-key.pem root@192.168.88.34:/pki/private/$1-key.key
+scp -oStrictHostKeyChecking=no -i /root/.ssh/vsfikey /etc/ipsec.d/certs/$1-cert.pem root@192.168.88.34:/pki/certs/$1-cert.cert
